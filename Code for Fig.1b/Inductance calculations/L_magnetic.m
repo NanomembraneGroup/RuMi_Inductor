648a11a9f6eaaea77d3741eb04f6cd03f05eac39
NanomembraneGroup <stellacc458@gmail.com>
2025-03-03 17:59:46 +0800

    Update L_magnetic.m

--- Code for Fig.1b/Inductance calculations/L_magnetic.m
+++ Code for Fig.1b/Inductance calculations/L_magnetic.m
@@ -1,5 +1,5 @@
 function L = L_magnetic(g,tm,w,mu_r,l)
-mu_0 = 4*pi*10^-7; % 真空的磁导率
+mu_0 = 4*pi*10^-7;
 lambda = sqrt(g * tm * mu_r / 2);
 tanh_term = tanh(w / (2*lambda));
 L = (mu_0 * mu_r * tm *l / (4*w)) * (1 - (2*lambda/w) * tanh_term);
